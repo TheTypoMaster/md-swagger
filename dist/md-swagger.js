dc47b0e477eb0de3a9185314e6cb57489e55a210
Karthik Vellaichamy <karthikeyan.x.vellaichamy@verizon.com>
2015-08-04 16:49:22 -0500

    cors proxy redirection

--- dist/md-swagger.js
+++ dist/md-swagger.js
@@ -4,26 +4,7 @@
  * @link http://swagger.io
  * @license Apache-2.0
  */
-(function(){/*
-/// <reference path="../app.d.ts" />
-
-(function(){
-'use strict';
-	angular
-		.module('uiApp.docs',['ui.router'])
-		.config(config);
-
-	function config($urlRouterProvider,$stateProvider) {
-		$urlRouterProvider.otherwise('/apidoc');
-		$stateProvider
-			.state('apidoc', {
-				url: '/apidoc',
-				templateUrl: 'apps/docs/docs.html'
-		});
-	}
-})();*/
-
-/// <reference path="../app.d.ts" />
+(function(){/// <reference path="../app.d.ts" />
 
 (function () {
 	'use strict';
